0700bf3dafd714d856dc1393feda6eeb5e71ddf9
Zadkiel Aharonian <hello@zadkiel.fr>
2023-08-04 13:48:56 +0000

    fix: reduce interval to 15s
    Signed-off-by: GitHub <noreply@github.com>

--- buffer-size.sh
+++ buffer-size.sh
@@ -20,5 +20,5 @@ while true; do
     echo -e "$(find "${BUFFER_PATH}" -type f 2>/dev/null | wc -l)\t${BUFFER_PATH}" | sed -ne 's/\\/\\\\/;s/"/\\"/g;s/^\([0-9]\+\)\t\(.*\)$/logging_buffer_files{entity="\2", host="'$(hostname)'"} \1/p' >> /prometheus/node_exporter/textfile_collector/logging_buffer_files.prom.$$
     mv /prometheus/node_exporter/textfile_collector/logging_buffer_files.prom.$$ /prometheus/node_exporter/textfile_collector/logging_buffer_files.prom
 
-    sleep 30
+    sleep 15
 done
