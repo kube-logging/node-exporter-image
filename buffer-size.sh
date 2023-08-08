8a5d366adc5839adae1495501c27472c6753c793
Zadkiel Aharonian <hello@zadkiel.fr>
2023-08-08 13:57:52 +0200

    Update buffer-size.sh

--- buffer-size.sh
+++ buffer-size.sh
@@ -5,7 +5,7 @@
 while true; do
     # Deprecated: node_buffer_size_bytes will soon be replaced by logging_buffer_size_bytes
     # logging_buffer_size_bytes includes the host label
-    echo "# HELP node_buffer_size_bytes Disk space used [deprecated]" > /prometheus/node_exporter/textfile_collector/buffer_size.prom
+    echo "# HELP node_buffer_size_bytes Disk space used" > /prometheus/node_exporter/textfile_collector/buffer_size.prom
     echo "# TYPE node_buffer_size_bytes gauge" >> /prometheus/node_exporter/textfile_collector/buffer_size.prom
     du -sb ${BUFFER_PATH} | sed -ne 's/\\/\\\\/;s/"/\\"/g;s/^\([0-9]\+\)\t\(.*\)$/node_buffer_size_bytes{entity="\2"} \1/p' >> /prometheus/node_exporter/textfile_collector/buffer_size.prom.$$
     mv /prometheus/node_exporter/textfile_collector/buffer_size.prom.$$ /prometheus/node_exporter/textfile_collector/buffer_size.prom
